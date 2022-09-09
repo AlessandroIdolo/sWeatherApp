e4fc2d660dc7809c809644db98e38b8e22360d8c
Alessandro Idolo <ale.idolo@gmail.com>
2022-09-09 18:42:03 +0200

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiBaseUrl: 'https://yahoo-weather5.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'yahoo-weather5.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '3f73f7c55amsh69dcfba159d0d12p1d4bf2jsn81db1eb3a582'
+
 };
